1454ac3331d50976610887343ee6d188a187d0bd
Cristian <ape4p2diaz.cristian@gmail.com>
2023-12-18 13:55:26 -0600

    updates

--- src/Components/InfoCard/InfoCard.tsx
+++ src/Components/InfoCard/InfoCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconDefinition } from '@fortawesome/free-regular-svg-icons';
 import './InfoCard.css';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 export interface InfoCardProps {
 	icon: IconDefinition;
@@ -14,7 +15,7 @@ export const InfoCard = ({ icon, header, body }: InfoCardProps) => {
 		<div className="InfoCard">
 			<div className="info-card-header">
 				<FontAwesomeIcon
-					icon={icon}
+					icon={icon as IconProp}
 					size="2x"
 					className="info-card-header-logo"
 				/>
